1b53a02f2c8308671a3db2160bb02547bb131bca
zero <a0970937127@gmail.com>
2022-10-22 01:06:27 +0800

    update.

--- src/Requests/Parameters/EcPay/ChoicePayments/ATM.php
+++ src/Requests/Parameters/EcPay/ChoicePayments/ATM.php
@@ -2,7 +2,7 @@
 
 namespace Zero\Requests\Parameters\EcPay\ChoicePayments;
 
-use Zero\Request\Parameters\EcPay\ChoicePayments\Base;
+use Zero\Requests\Parameters\EcPay\ChoicePayments\Base;
 
 class ATM extends Base
 {
